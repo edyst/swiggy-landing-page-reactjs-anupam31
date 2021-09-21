6ab12a5370976f176a27c114a2bb82e57aa01261
Anupam Kumar <89263010+anupam31@users.noreply.github.com>
2021-09-21 22:41:03 +0530

    React Project

--- src/components/resturantsection/resturant.js
+++ src/components/resturantsection/resturant.js
@@ -9,9 +9,9 @@ function Resturant()
           <div className="row">
             <div className="col-sm-1"></div>
             <div className="col-sm-5">
-            <h2 class="reshead1">Restaurants in </h2>
-            <h2 class="reshead2">your pocket</h2>
-            <div class="div1">Order from your favorite restaurants & track on the go, with the all-new Swiggy app.</div>
+            <h2 class="rest1">Restaurants in </h2>
+            <h2 class="rest2">your pocket</h2>
+            <div class="division1">Order from your favorite restaurants & track on the go, with the all-new Swiggy app.</div>
             <nav style={{display:"flex",gap:"20px"}}>
                 <a href="https://web.archive.org/web/20210903174711/https://play.google.com/store/apps/details?id=in.swiggy.android"><img class="restimg1" width="200" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_108/play_ip0jfp" alt=""/></a>
                 <a href="https://web.archive.org/web/20201111215216/https://itunes.apple.com/in/app/swiggy-food-order-delivery/id989540920"><img class="restimg2" width="200" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_108/iOS_ajgrty" alt=""/></a>
