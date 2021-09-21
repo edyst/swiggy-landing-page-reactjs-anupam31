7858bee4d154745dbd34a22a533cea29f1c0714b
Anupam Kumar <89263010+anupam31@users.noreply.github.com>
2021-09-21 22:25:36 +0530

    Update feature.js

--- src/components/feature/feature.js
+++ src/components/feature/feature.js
@@ -9,25 +9,25 @@ function Feature()
           <div className="row">
             <div className="col-sm-4">
               <div className="row">
-              <img class="optionimg1" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_210,h_398/4x_-_No_min_order_x0bxuf" alt=""/>
-              <h2 class="optiondiv1">No Minimum Order</h2>
-              <div class="optiondiv2">Order in for yourself or for the group, with no restrictions on order value</div>
+              <img class="opt-img1" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_210,h_398/4x_-_No_min_order_x0bxuf" alt=""/>
+              <h2 class="opt-div1">No Minimum Order</h2>
+              <div class="opt-div2">Order in for yourself or for the group, with no restrictions on order value</div>
               </div>
             </div>
 
 
             <div className="col-sm-4">
             <div className="row">
-              <img class="optionimg1" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_224,h_412/4x_Live_order_zzotwy" alt=""/>
-              <h2 class="optiondiv1">Live Order Tracking</h2>
-              <div class="optiondiv2">Know where your order is at all times,</div><div class="featurediv2">from the restaurant to your doorstep</div>
+              <img class="opt-img1" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_224,h_412/4x_Live_order_zzotwy" alt=""/>
+              <h2 class="opt-div1">Live Order Tracking</h2>
+              <div class="opt-div2">Know where your order is at all times,</div><div class="featurediv2">from the restaurant to your doorstep</div>
               </div>
             </div>
 
             <div className="col-sm-4">
-            <img class="optionimg3" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_248,h_376/4x_-_Super_fast_delivery_awv7sn"  alt=""/>
-            <h2 class="optiondiv1">Lightning-Fast Delivery</h2>
-            <div class="optiondiv2">Experience Swiggy's superfast delivery</div><div class="featurediv2">for food delivered fresh & on time</div>
+            <img class="opt-img1" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_248,h_376/4x_-_Super_fast_delivery_awv7sn"  alt=""/>
+            <h2 class="opt-div1">Lightning-Fast Delivery</h2>
+            <div class="opt-div2">Experience Swiggy's superfast delivery</div><div class="featurediv2">for food delivered fresh & on time</div>
             </div>
 
             
